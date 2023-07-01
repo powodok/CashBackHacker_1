fe03ae9654b36ed8069dfc8adbe2969137e20c4b
Nikita Ivanov <resay99@mail.ru>
2023-07-01 15:35:31 +0400

    junit4new

---
+++ src/test/java/CashbackHackServiceTest_2.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Test;
+import ru_netology.CashbackHackService;
+
+public class CashbackHackServiceTest_2 {
+    @Test
+    public void testRemainShouldReturnZeroWhenAmountIsMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 3000;
+        int expectedRemain = 1000;
+
+        int actualRemain = service.remain(amount);
+
+        Assert.assertEquals(expectedRemain, actualRemain);
+    }
+
+    @Test
+    public void testRemainShouldReturnRemainderWhenAmountIsNotMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2500;
+        int expectedRemain = 500;
+
+        int actualRemain = service.remain(amount);
+
+        Assert.assertEquals(expectedRemain, actualRemain);
+    }
+
+    @Test
+    public void testRemainShouldReturnBoundaryWhenAmountIsZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expectedRemain = 1000;
+
+        int actualRemain = service.remain(amount);
+
+        Assert.assertEquals(expectedRemain, actualRemain);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testRemainShouldThrowExceptionWhenAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        service.remain(amount);
+    }
+}
